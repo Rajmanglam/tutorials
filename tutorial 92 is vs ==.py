9e7e0d993d1d9a947ae16edb529c9da4da26ea75
rajmanglam <rajmanglam111@gmail.com>
2021-02-07 09:32:42 +0530

    a

---
+++ tutorial 92 is vs ==.py
@@ -0,0 +1,9 @@
+
+
+# == - value equality - Two objects have the same value//ya phir dono ka value ek hai
+# is - reference equality - Two references refer to the same object// dono ek hi hai
+
+a =[6, 4 , "34"]
+b = [6, 4 , "34"]
+print(b==a)#will give true
+print(b is a)#will give false
